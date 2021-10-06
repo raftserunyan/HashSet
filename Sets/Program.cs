a3a21e99d5c1a2a9c67083c194d982f4e6dcd0e8
Rafik Tserunyan <rafik.tserunyan@dataart.com>
2021-10-06 18:01:22 +0400

    Add project files.

---
+++ Sets/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<int> hs = new HashSet<int>();
+
+			hs.Add(112);
+			hs.Add(0);
+			hs.Add(11);
+
+			hs.Contains(0);
+		}
+	}
+}
